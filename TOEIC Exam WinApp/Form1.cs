a27f0e335b5a9f962cc4f101ff83c127ba0c511b
Nguyen The Hao <thehaohcm@yahoo.com.vn>
2021-05-02 20:04:55 +0700

    add .gitignore file and add event auto close when browser redirect to finish page

--- TOEIC Exam WinApp/Form1.cs
+++ TOEIC Exam WinApp/Form1.cs
@@ -30,10 +30,11 @@ namespace TOEIC_Exam_WinApp
             string url = "http://51.222.85.96:8080";
             if (!String.IsNullOrEmpty(Name) && !String.IsNullOrEmpty(eMail))
             {
-                if (Name.Contains(".")){
+                if (Name.Contains("."))
+                {
                     string FirstName = Name.Substring(0, Name.IndexOf("."));
                     string LastName = Name.Substring(Name.IndexOf(".") + 1);
-                    FirstName=char.ToUpper(FirstName[0]) + FirstName.Substring(1);
+                    FirstName = char.ToUpper(FirstName[0]) + FirstName.Substring(1);
                     LastName = char.ToUpper(LastName[0]) + LastName.Substring(1);
                     Name = FirstName + " " + LastName;
                 }
@@ -46,6 +47,8 @@ namespace TOEIC_Exam_WinApp
 
             chromiumWebBrowser = new ChromiumWebBrowser(url);
 
+            chromiumWebBrowser.AddressChanged += OnBrowserAddressChanged;
+
             this.Controls.Add(chromiumWebBrowser);
             chromiumWebBrowser.Dock = DockStyle.Fill;
         }
@@ -53,6 +56,15 @@ namespace TOEIC_Exam_WinApp
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Cef.Shutdown();
+        }
+
+        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            //this.InvokeOnUiThreadIfRequired(() => Text = e.Address);
+
+            if (e.Address.Equals(url + "/finish?close=true"){
+                Application.Exit();
+            }
         }
     }
 }
